6af03dcc921a6c9c17684ff0a417eed350b027b5
Lê Ngọc Hà <43954353+ha1999@users.noreply.github.com>
2022-05-06 07:24:40 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-alert('What is your name')
+const para = document.createElement("p");
+const node = document.createTextNode("This is a paragraph.");
+
+para.appendChild(node);
+document.getElementById("myDIV").appendChild(para);
